a5d8847c32429bbe26d642a4fa5f8cc08e19033e
mynameisvinn <vin.tang@gmail.com>
2020-12-04 13:48:45 -0500

    fix unittest

--- tests/test_count_faces.py
+++ tests/test_count_faces.py
@@ -5,8 +5,8 @@ from FaceOff import count_faces
 class TestCount(TestCase):
 
     def test_count_faces(self):
-    	"""counts for not-static images should equal zero.
-    	"""
+        """counts for not-static images should equal zero.
+        """
         folder = "dataset/547538174468711490516541559363"
         self.assertEqual(count_faces(folder, label="not_static"), 0)
 
